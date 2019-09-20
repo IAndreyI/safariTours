13fb8d34050a34a0c9f0f6175da0182386412e09
Andrey <1993czar@gmail.com>
2019-09-20 18:30:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+	$('.slider').slick({
+		appendArrows: '#slider-nav',
+		prevArrow: '<div class="slick-prev"><div class = "triangle"></div></div>',
+		nextArrow: '<div class="slick-next"><div class = "triangle"></div></div>',
+	});
+});
